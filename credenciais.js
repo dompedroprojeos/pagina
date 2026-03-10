1fa7276201f301c58b7e959835a559900c8d8022
dompedroprojeos <147060579+dompedroprojeos@users.noreply.github.com>
2026-03-10 09:03:19 -0300

    Add files via upload

---
+++ credenciais.js
@@ -0,0 +1,27 @@
+// credenciais.js - Arquivo separado com as credenciais em Base64
+const usuarios = {
+    'julia.silva@rededompedro.com': {
+        nome: 'Júlia Silva',
+        // senha original: julia@DP@123456
+        senha: 'anVsaWFARFBAQDEyMzQ1Ng==',
+        tipo: 'usuario'
+    },
+    'guilherme.araujo@rededompedro.com': {
+        nome: 'Guilherme Araújo',
+        // senha original: guilherme@DP@123456
+        senha: 'Z3VpbGhlcm1lQERQQDEyMzQ1Ng==',
+        tipo: 'usuario'
+    },
+    'lilian.borges@rededompedro.com': {
+        nome: 'Lilian Borges',
+        // senha original: lilian@DP@123456
+        senha: 'bGlsaWFuQERQQDEyMzQ1Ng==',
+        tipo: 'usuario'
+    },
+    'waldeci.ramos@rededompedro.com': {
+        nome: 'Waldeci Ramos',
+        // senha original: ADM@2026
+        senha: 'QURNQDIwMjY=',
+        tipo: 'admin'
+    }
+};
